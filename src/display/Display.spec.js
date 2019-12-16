3349c071562a30dbb5eef2903acc2f6f0e67a8ee
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-16 18:43:39 -0500

    add todo tests

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -4,12 +4,24 @@ import '@testing-library/jest-dom/extend-expect';
 
 import Display from './Display';
 
-describe ('the Display component', () => {
+describe (`the Display component`, () => {
 
-  test ('renders without crashing', () => {
+  test (`renders without crashing`, () => {
     const { unmount } = render (<Display/>);
 
     unmount ();
   });
+
+  test.todo (`displays open/closed`);
+
+  test.todo (`displays locked/unlocked`);
+
+  test.todo (`displays 'Closed' when \`closed\`, else 'Open'`);
+
+  test.todo (`displays 'Locked' when \`locked\`, else 'Unlocked'`);
+
+  test.todo (`uses 'red-led' class when \`closed\` or \`locked\``);
+
+  test.todo (`uses 'green-led' class when \`open\` or \`unlocked\``);
 
 });
