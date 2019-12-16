a9ddd0b7ac31f8bc7c4650de3997f96fdf8157b5
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-16 18:26:21 -0500

    test Dashboard contents

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,25 @@
-// Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Dashboard from './Dashboard';
+
+describe ('the Dashboard component', () => {
+  test ('renders', () => {
+    render (<Dashboard/>);
+  })
+  test ('shows the display', () => {
+    const { getByTestId } = render (<Dashboard/>);
+    const Display = getByTestId ('display-panel');
+
+    expect (Display).toBeInTheDocument ();
+    expect (Display).toHaveClass ('display panel');
+  });
+  test ('shows the controls', () => {
+    const { getByTestId } = render (<Dashboard/>);
+    const Controls = getByTestId ('controls-panel');
+
+    expect (Controls).toBeInTheDocument ();
+    expect (Controls).toHaveClass ('controls panel');
+  });
+});
