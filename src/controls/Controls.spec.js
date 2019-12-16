2e5018fc121b8c07bcf7723de61c2dc8d092c6c4
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-16 18:35:21 -0500

    add simple render test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,15 @@
-// Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Controls from './Controls';
+
+describe ('the Controls component', () => {
+
+  test ('renders without crashing', () => {
+    const { unmount } = render (<Controls/>);
+
+    unmount ();
+  });
+
+});
