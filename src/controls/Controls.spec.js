160cf9bc2a278d5b8230bdf2338fd0b6f6f8fb15
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-16 18:53:18 -0500

    add tests todo

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -4,12 +4,24 @@ import '@testing-library/jest-dom/extend-expect';
 
 import Controls from './Controls';
 
-describe ('the Controls component', () => {
+describe (`the Controls component`, () => {
 
-  test ('renders without crashing', () => {
+  test (`renders without crashing`, () => {
     const { unmount } = render (<Controls/>);
 
     unmount ();
   });
+
+  test.todo (`has button to toggle \`closed\``);
+
+  test.todo (`has button to toggle \`locked\``);
+
+  test.todo (`the \`closed\` toggler text changes to reflect action`);
+
+  test.todo (`the \`locked\` toggler text changes to reflect action`);
+
+  test.todo (`the \`closed\` toggler is disabled when \`locked\``);
+
+  test.todo (`the \`locked\` toggler is disabled when not \`closed\``);
 
 });
