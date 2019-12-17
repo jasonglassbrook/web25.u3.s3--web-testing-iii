739cb72745839fdba7f58b927d1735cf89abc197
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-16 19:11:02 -0500

    test Display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -12,16 +12,58 @@ describe (`the Display component`, () => {
     unmount ();
   });
 
-  test.todo (`displays open/closed`);
+  test (`displays whether closed/open`, () => {
+    const { getByTestId } = render (<Display/>);
+    const IsClosed = getByTestId ('is-closed');
 
-  test.todo (`displays locked/unlocked`);
+    expect (IsClosed).toBeInTheDocument ();
+  });
 
-  test.todo (`displays 'Closed' when \`closed\`, else 'Open'`);
+  test (`displays whether locked/unlocked`, () => {
+    const { getByTestId } = render (<Display/>);
+    const IsLocked = getByTestId ('is-locked');
 
-  test.todo (`displays 'Locked' when \`locked\`, else 'Unlocked'`);
+    expect (IsLocked).toBeInTheDocument ();
+  });
 
-  test.todo (`uses 'red-led' class when \`closed\` or \`locked\``);
+  test (`displays 'Closed' when \`closed\`, else 'Open'`, () => {
+    const { getByTestId, rerender } = render (<Display closed={true}/>);
+    const IsClosed = getByTestId ('is-closed');
 
-  test.todo (`uses 'green-led' class when \`open\` or \`unlocked\``);
+    expect (IsClosed).toHaveTextContent ('Closed');
+
+    rerender (<Display closed={false}/>);
+
+    expect (IsClosed).toHaveTextContent ('Open');
+  });
+
+  test (`displays 'Locked' when \`locked\`, else 'Unlocked'`, () => {
+    const { getByTestId, rerender } = render (<Display locked={true}/>);
+    const IsLocked = getByTestId ('is-locked');
+
+    expect (IsLocked).toHaveTextContent ('Locked');
+
+    rerender (<Display locked={false}/>);
+
+    expect (IsLocked).toHaveTextContent ('Unlocked');
+  });
+
+  test (`uses 'red-led' class when \`closed\` or \`locked\``, () => {
+    const { getByTestId } = render (<Display closed={true} locked={true}/>);
+    const IsClosed = getByTestId ('is-closed');
+    const IsLocked = getByTestId ('is-locked');
+
+    expect (IsClosed).toHaveClass ('red-led');
+    expect (IsLocked).toHaveClass ('red-led');
+  });
+
+  test (`uses 'green-led' class when \`open\` or \`unlocked\``, () => {
+    const { getByTestId } = render (<Display closed={false} locked={false}/>);
+    const IsClosed = getByTestId ('is-closed');
+    const IsLocked = getByTestId ('is-locked');
+
+    expect (IsClosed).toHaveClass ('green-led');
+    expect (IsLocked).toHaveClass ('green-led');
+  });
 
 });
